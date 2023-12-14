6fca455497e647e9914e9993a418122f7bd9c7a7
unknown <thomas.piquet9@gmail.com>
2023-12-14 15:41:56 +0100

    refactor: refactor implementation for range splitting feature

--- src/prime-numbers/prime-numbers.service.ts
+++ src/prime-numbers/prime-numbers.service.ts
@@ -4,6 +4,7 @@ import { NumberRange } from "../models/number-range.model";
 @Injectable()
 export class PrimeNumbersService {
     public readonly MAXIMUM_RANGE_SIZE: number = 50000;
+    public readonly RANGE_STEP: number = 5000;
 
     public truncateRangeIfTooLarge(range: NumberRange): NumberRange {
         if (range.end - range.start > this.MAXIMUM_RANGE_SIZE) {
@@ -13,21 +14,29 @@ export class PrimeNumbersService {
     }
 
     public splitRangeOfNumberIntoSmallerSubRange(range: NumberRange): NumberRange[] {
-        if (range.end - range.start < 5000 - 1) {
-            const startAt: number = Math.floor(range.start / 5000) * 5000;
-            return [new NumberRange(startAt, startAt + 5000 - 1)];
+        if (range.end - range.start < this.RANGE_STEP - 1) {
+            const startAt: number = this.getStandardisedRangeStart(range.start);
+            return [new NumberRange(startAt, startAt + this.RANGE_STEP - 1)];
         }
 
-        const subRangeCount: number = Math.ceil((range.end - range.start) / 5000);
-        const startAt: number = Math.floor(range.start / 5000) * 5000;
+        const subRangeCount: number = this.getSubRangeCountFromRange(range);
+        const startAt: number = this.getStandardisedRangeStart(range.start);
 
         let subRanges: NumberRange[] = [];
 
         for (let subRangeIndex = 0; subRangeIndex < subRangeCount; ++subRangeIndex) {
-            subRanges.push(new NumberRange(startAt + 5000 * subRangeIndex,
-                startAt + 5000 * subRangeIndex + 5000 - 1))
+            subRanges.push(new NumberRange(startAt + this.RANGE_STEP * subRangeIndex,
+                startAt + this.RANGE_STEP * subRangeIndex + this.RANGE_STEP - 1))
         }
 
         return subRanges;
+    }
+
+    private getStandardisedRangeStart(start: number): number {
+        return Math.floor(start / this.RANGE_STEP) * this.RANGE_STEP;
+    }
+
+    private getSubRangeCountFromRange(range: NumberRange): number {
+        return Math.ceil((range.end - range.start) / this.RANGE_STEP);
     }
 }

a7ca5a99e3841cbdf69b6fa8b3ca222834f9ecff
unknown <thomas.piquet9@gmail.com>
2023-12-14 14:52:40 +0100

    refactor: refactor implementation for truncate too large range feature

--- src/prime-numbers/prime-numbers.service.ts
+++ src/prime-numbers/prime-numbers.service.ts
@@ -3,9 +3,11 @@ import { NumberRange } from "../models/number-range.model";
 
 @Injectable()
 export class PrimeNumbersService {
+    public readonly MAXIMUM_RANGE_SIZE: number = 50000;
+
     public truncateRangeIfTooLarge(range: NumberRange): NumberRange {
-        if (range.end - range.start > 50000) {
-            range.end = range.start + 50000;
+        if (range.end - range.start > this.MAXIMUM_RANGE_SIZE) {
+            range.end = range.start + this.MAXIMUM_RANGE_SIZE;
         }
         return range;
     }
